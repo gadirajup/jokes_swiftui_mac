d99a07eec5cd4ad7b5ea108e13ed3d32a26b88ec
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-09-22 21:06:39 -0700

    Initial Commit

---
+++ Jokers_Mac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Jokers_Mac
+//
+//  Created by Prudhvi Gadiraju on 9/22/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
